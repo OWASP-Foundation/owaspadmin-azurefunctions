49a0d359eef94a8229324feb37a6ff8c2b7fe1ab
hblankenship <harold.blankenship@owasp.org>
2020-01-15 09:29:55 -0600

    else colon

--- SharedCode/wufoo.py
+++ SharedCode/wufoo.py
@@ -50,7 +50,7 @@ class OWASPWufoo:
             if len(jsonEntries) > 0: 
                 jsonEntry = jsonEntries['Entries'][0]
                 result = jsonEntry[fieldid]
-        else
+        else:
             logging.error(f'Wufoo returned {r.text}')
         return result
 
@@ -69,7 +69,7 @@ class OWASPWufoo:
                     result = jsonEntry[self.STATUS_FIELD]
                     if 'Completed' in result:
                         break
-        else
+        else:
             logging.error(f'Wufoo returned {r.text}')
 
         return result
