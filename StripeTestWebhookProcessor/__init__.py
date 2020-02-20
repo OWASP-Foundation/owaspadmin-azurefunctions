a072176a86bc07058c51c5a638885997845b96ce
Simon <simon.sorich@gmail.com>
2020-02-20 12:52:51 -0800

    Changing Event.construct_from syntax... again

--- StripeTestWebhookProcessor/__init__.py
+++ StripeTestWebhookProcessor/__init__.py
@@ -25,7 +25,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         event = stripe.Event.construct_from(
-            payload
+            payload, os.environ["STRIPE_TEST_SECRET"]
         )
     except ValueError as e:
         return func.HttpResponse(status_code=400)
