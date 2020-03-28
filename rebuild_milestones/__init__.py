cc47f8d59e4bcb00973045b94bee3ee4e1d1f23d
Harold Blankenship <hblankenship@pop-os.localdomain>
2020-03-28 15:26:11 -0500

    logging referer

--- rebuild_milestones/__init__.py
+++ rebuild_milestones/__init__.py
@@ -11,6 +11,8 @@ from urllib.parse import unquote_plus
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    logging.info(req.headers['referer'])
+    
      # first check for a valid call
     body = req.get_body()
     
