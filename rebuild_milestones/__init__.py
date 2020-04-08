2b9b497ebb302df5de8f324b953da3b82e25c705
Harold Blankenship <hblankenship@pop-os.localdomain>
2020-04-08 12:46:58 -0500

    remove test

--- rebuild_milestones/__init__.py
+++ rebuild_milestones/__init__.py
@@ -11,8 +11,6 @@ from urllib.parse import unquote_plus
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    logging.info(req.headers['referer'])
-    
      # first check for a valid call
     body = req.get_body()
     
