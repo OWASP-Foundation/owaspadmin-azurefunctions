0c59cc72bc57b7966c59c0ba9ee730d530032049
Harold Blankenship <harold.blankenship@owasp.com>
2021-03-01 13:28:15 -0600

    update slack invite code

--- owasp_slack_add_user/__init__.py
+++ owasp_slack_add_user/__init__.py
@@ -11,16 +11,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         request = req.get_json()
         email = request.get('email', None)
 
-    invurl = "https://slack.com/api/users.admin.invite"
+    invurl = "https://owasp.slack.com/api/users.admin.invite"
     data = {
         'email':email,
-        'token': os.environ['SL_ACCESS_TOKEN'],
-        'set_active': true
+        'token': os.environ['SL_UINV_TOKEN'],
+        'set_active': True
     }
-    headers = {'content-type':'application/json; charset=utf-8', 'Authorization':f'Bearer {os.environ["SL_ACCESS_TOKEN"]}' }
+    #headers = {'content-type':'application/json; charset=utf-8', 'Authorization':f'Bearer {os.environ["SL_ACCESS_TOKEN"]}' }
 
     # respond to caller...
-    r = requests.post(url = invurl, headers=headers, data=data)
+    r = requests.post(url = invurl, data=data)
     
     return func.HttpResponse(r.text, status_code = r.status_code)
     
