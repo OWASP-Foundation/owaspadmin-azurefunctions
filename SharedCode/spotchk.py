655846d91c64a5be6e4b57aa7e6a3cfa0ad23ebf
Harold Blankenship <harold.blankenship@owasp.com>
2021-03-08 11:52:46 -0600

    fix class

--- SharedCode/spotchk.py
+++ SharedCode/spotchk.py
@@ -22,9 +22,9 @@ class spotchk:
         # minor annoyance for unofficial callers
         return testsec == salt_secret and testprov.find(salt_provider) >= 0 and testla == salt_la and sl_token == md5_token
 
-def validate_query2(self, md5str):
+    def validate_query2(self, md5str):
         try:
-            salt_secret = os.environ['SL_TEAM']
+            salt_secret = os.environ['SL_TEAM_GENERAL']
             salt_provider = os.environ['SL_RURL']
             
             testsec = md5str[os.environ['P_SECRET']]
@@ -33,7 +33,7 @@ def validate_query2(self, md5str):
             salt_la = os.environ['SL_LAV']
             testla =  os.environ['P_LAV']
 
-            sl_token = os.environ["SL_TOKEN"]
+            sl_token = os.environ["SL_TOKEN_GENERAL"]
             md5_token = md5str[os.environ["SL_CHECK"]]
 
             sl_sgchannel = os.environ['SL_STAFF_GENERAL']
