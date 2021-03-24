694ff6555c2b543d90ab65d24da8dc498c77cbb0
Harold Blankenship <harold.blankenship@owasp.com>
2021-03-23 21:54:16 -0500

    update copper opportunities

--- SharedCode/copper.py
+++ SharedCode/copper.py
@@ -79,7 +79,7 @@ class OWASPCopper:
     cp_opportunity_autorenew_checkbox = 419575
     cp_opportunity_invoice_no = 407333  # can be the URL to the stripe payment for membership
     cp_opportunity_pipeline_id_membership = 721986
-    
+
     #leader specific
     
     def GetHeaders(self):
@@ -103,17 +103,24 @@ class OWASPCopper:
         
         return ''
         
-    def ListOpportunities(self):
+    def ListOpportunities(self, page_number=1, pipeline_ids=None, status_ids=[0,1,2,3]):
         data = {
             'page_size': 200,
-            'sort_by': 'name'
+            'sort_by': 'name',
+            'page_number':page_number,
+            'status_ids':status_ids
         }
+
+        if pipeline_ids:
+            data['pipeline_ids'] = pipeline_ids
+
         url = f'{self.cp_base_url}{self.cp_opp_fragment}{self.cp_search_fragment}'
         r = requests.post(url, headers=self.GetHeaders(), data=json.dumps(data))
         if r.ok:
             return r.text
         
         return ''
+        
     def GetPerson(self, pid):
         url = f"{self.cp_base_url}{self.cp_people_fragment}{pid}"
         r = requests.get(url, headers = self.GetHeaders())
