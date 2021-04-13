b7ab7ad903858eb72e1830431005963d531d6944
Harold Blankenship <harold.blankenship@owasp.com>
2021-04-12 21:09:23 -0500

    split build static files

--- BuildStaticWebsiteFiles/__init__.py
+++ BuildStaticWebsiteFiles/__init__.py
@@ -5,83 +5,7 @@ import re
 import azure.functions as func
 from ..SharedCode import github
 from ..SharedCode import helperfuncs
-from ..SharedCode import meetup
-
 import base64
-
-def parse_leaderline(line):
-    ename = line.find(']')
-    name = line[line.find('[') + 1:line.find(']')]
-    email = line[line.find('(', ename) + 1:line.find(')', ename)]
-    return name, email
-
-def add_to_leaders(repo, content, all_leaders, stype):
-    lines = content.split('\n')
-    max_leaders = 5
-    leader_count = 0
-    in_leaders = False
-    for line in lines:
-        testline = line.lower()
-        if in_leaders and leader_count > 0 and not testline.startswith('*'):
-            break
-        
-        if(testline.startswith('###') and 'leader' not in testline):
-            break
-        elif testline.startswith('###') and 'leader' in testline:
-            in_leaders = True
-            continue
-
-        fstr = line.find('[')
-        if(line.startswith('*') and fstr > -1 and fstr < 4):
-            name, email = parse_leaderline(line)
-            if 'leader.email@owasp.org' not in email and leader_count < max_leaders: # default
-                leader = {}
-                leader['name'] = name
-                leader['email'] = email.replace('mailto://', '').replace('mailto:','').lower()
-                leader['group'] = repo['title']
-                leader['group-type'] = stype
-                leader['group_url'] = repo['url']
-                
-                all_leaders.append(leader)
-                leader_count = leader_count + 1
-
-
-def build_leaders_json(gh):
-    all_leaders = []
-    repos = gh.GetPublicRepositories('www-')
-    for repo in repos:
-        stype = ''
-        if repo['name'] == 'www-projectchapter-example':
-            continue
-        
-        if 'www-chapter' in repo['url']:
-            stype = 'chapter'
-        elif 'www-committee' in repo['url']:
-            stype = 'committee'
-        elif 'www-project' in repo['url']:
-            stype = 'project'
-        else:
-            continue
-
-        r = gh.GetFile(repo['name'], 'leaders.md')
-        if r.ok:
-            doc = json.loads(r.text)
-            content = base64.b64decode(doc['content']).decode(encoding='utf-8')
-
-            add_to_leaders(repo, content, all_leaders, stype)
-    
-    r = gh.GetFile('owasp.github.io', '_data/leaders.json')
-    sha = ''
-    if r.ok:
-        doc = json.loads(r.text)
-        sha = doc['sha']
-    
-    r = gh.UpdateFile('owasp.github.io', '_data/leaders.json', json.dumps(all_leaders, ensure_ascii=False, indent = 4), sha)
-    if r.ok:
-        logging.info('Update leaders json succeeded')
-    else:
-        logging.error(f'Update leaders json failed: {r.status}')
-
 
 def build_committee_json(gh):
     repos = gh.GetPublicRepositories('www-committee')
@@ -193,39 +117,6 @@ def build_chapter_json(gh):
     else:
         logging.error(f"Failed to update _data/chapters.json: {r.text}")
 
-def build_inactive_chapters_json(gh):
-    repos = gh.GetInactiveRepositories('www-chapter')
-    fmt_str = "%a %b %d %H:%M:%S %Y"
-    for repo in repos:
-        repo['name'] = repo['name'].replace('www-chapter-','').replace('-', ' ')
-        repo['name'] = " ".join(w.capitalize() for w in repo['name'].split())
-        try:
-            dobj = datetime.datetime.strptime(repo['created'], fmt_str)
-            repo['created'] = dobj.strftime("%Y-%m-%d")
-        except ValueError:
-            pass
-        try:
-            dobj = datetime.datetime.strptime(repo['updated'], fmt_str)
-            repo['updated'] = dobj.strftime("%Y-%m-%d")
-        except ValueError:
-            pass
-
-    repos.sort(key=lambda x: x['name'])
-    repos.sort(key=lambda x: x['region'], reverse=True)
-   
-    sha = ''
-    r = gh.GetFile('owasp.github.io', '_data/inactive_chapters.json')
-    if gh.TestResultCode(r.status_code):
-        doc = json.loads(r.text)
-        sha = doc['sha']
-
-    contents = json.dumps(repos)
-    r = gh.UpdateFile('owasp.github.io', '_data/inactive_chapters.json', contents, sha)
-    if gh.TestResultCode(r.status_code):
-        logging.info('Updated _data/inactive_chapters.json successfully')
-    else:
-        logging.error(f"Failed to update _data/inactive_chapters.json: {r.text}")
-
 def update_chapter_admin_team(gh):
     team_id = gh.GetTeamId('chapter-administration')
     if team_id:
@@ -284,116 +175,34 @@ def update_corp_members(gh):
     else:
         logging.error(f'Failed to update assets/sitedata/corp_members.yml: {r.text}')
 
-def deEmojify(text):
-    EMOJI_PATTERN = re.compile(
-        "(["
-        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        "\U0001F300-\U0001F5FF"  # symbols & pictographs
-        "\U0001F600-\U0001F64F"  # emoticons
-        "\U0001F680-\U0001F6FF"  # transport & map symbols
-        "\U0001F700-\U0001F77F"  # alchemical symbols
-        "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-        "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-        "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-        "\U0001FA00-\U0001FA6F"  # Chess Symbols
-        "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-        "\U00002702-\U000027B0"  # Dingbats
-        "\U000024C2-\U0001F251" 
-        "])"
-    )
-
-    return EMOJI_PATTERN.sub(u'', text)
-
-def add_to_events(mue, events, repo):
+# def create_chapter_events(gh, mu):
+#     repos = gh.GetPublicRepositories('www-chapter')
     
-    if len(mue) <= 0 or 'errors' in mue:
-        return events
-    
-    chapter = repo.replace('www-chapter-','').replace('-', ' ')
-    chapter = " ".join(w.capitalize() for w in chapter.split())
-                
-    for mevent in mue:
-        event = {}
-        today = datetime.datetime.today()
-        eventdate = datetime.datetime.strptime(mevent['local_date'], '%Y-%m-%d')
-        tdelta = eventdate - today
-        if tdelta.days >= 0 and tdelta.days < 30:
-            event['chapter'] = chapter
-            event['repo'] = repo
-            event['name'] = mevent['name']
-            event['date'] = mevent['local_date']
-            event['time'] = mevent['local_time']
-            event['link'] = mevent['link']
-            event['timezone'] = mevent['group']['timezone']
-            if 'description' in mevent:
-                event['description'] = deEmojify(mevent['description'])
-            else:
-                event['description'] = ''
-                
-            events.append(event)
-
-    return events
-
-def create_community_events(gh, mu):
-    repos = gh.GetPublicRepositories('www-')
-    
-    events = []
-    for repo in repos:
-        if 'www-chapter' not in repo['name'] and 'www-project' not in repo['name'] and 'www-committee' not in repo['name']:
-            continue
-
-        if 'meetup-group' in repo and repo['meetup-group']:
-            if mu.Login():
-                mstr = mu.GetGroupEvents(repo['meetup-group'])
-                if mstr:
-                    muej = json.loads(mstr)
-                    add_to_events(muej, events, repo['name'])
+#     events = []
+#     for repo in repos:
+#         if 'meetup-group' in repo and repo['meetup-group']:
+#             if mu.Login():
+#                 mstr = mu.GetGroupEvents(repo['meetup-group'])
+#                 if mstr:
+#                     muej = json.loads(mstr)
+#                     add_to_events(muej, events, repo['name'])
                 
 
-    if len(events) <= 0:
-        return
+#     if len(events) <= 0:
+#         return
         
-    r = gh.GetFile('www-community', '_data/community_events.json')
-    sha = ''
-    if r.ok:
-        doc = json.loads(r.text)
-        sha = doc['sha']
+#     r = gh.GetFile('owasp.github.io', '_data/chapter_events.json')
+#     sha = ''
+#     if r.ok:
+#         doc = json.loads(r.text)
+#         sha = doc['sha']
     
-    contents = json.dumps(events, indents=4)
-    r = gh.UpdateFile('www-community', '_data/community_events.json', contents, sha)
-    if r.ok:
-        logging.info('Updated _data/community_events.json successfully')
-    else:
-        logging.error(f"Failed to update _data/community_events.json: {r.text}")
-
-def create_chapter_events(gh, mu):
-    repos = gh.GetPublicRepositories('www-chapter')
-    
-    events = []
-    for repo in repos:
-        if 'meetup-group' in repo and repo['meetup-group']:
-            if mu.Login():
-                mstr = mu.GetGroupEvents(repo['meetup-group'])
-                if mstr:
-                    muej = json.loads(mstr)
-                    add_to_events(muej, events, repo['name'])
-                
-
-    if len(events) <= 0:
-        return
-        
-    r = gh.GetFile('owasp.github.io', '_data/chapter_events.json')
-    sha = ''
-    if r.ok:
-        doc = json.loads(r.text)
-        sha = doc['sha']
-    
-    contents = json.dumps(events, indents=4)
-    r = gh.UpdateFile('owasp.github.io', '_data/chapter_events.json', contents, sha)
-    if r.ok:
-        logging.info('Updated _data/chapter_events.json successfully')
-    else:
-        logging.error(f"Failed to update _data/chapter_events.json: {r.text}")
+#     contents = json.dumps(events, indents=4)
+#     r = gh.UpdateFile('owasp.github.io', '_data/chapter_events.json', contents, sha)
+#     if r.ok:
+#         logging.info('Updated _data/chapter_events.json successfully')
+#     else:
+#         logging.error(f"Failed to update _data/chapter_events.json: {r.text}")
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -438,34 +247,8 @@ def main(mytimer: func.TimerRequest) -> None:
         build_committee_json(gh)
     except Exception as err:
         logging.error(f"Exception building committees json file: {err}")
-    
-    logging.info('Building leaders json file')
-    try:
-        build_leaders_json(gh)
-    except Exception as err:
-        logging.error(f"Exception updating leaders json file: {err}")
 
-    #logging.info('Updating chapter events')
-    #mu = meetup.OWASPMeetup()
-    #try:
-    #    create_chapter_events(gh, mu)
-    #except Exception as err:
-    #    logging.error(f"Exception updating chapter events: {err}")
-    
-    logging.info('Updating community events')
-    mu = meetup.OWASPMeetup()
-    try:
-        create_community_events(gh, mu)
-    except Exception as err:
-        logging.error(f"Exception updating community events: {err}")
-
-    logging.info('Updating inactive chapters')
-    try:
-        build_inactive_chapters_json(gh)
-    except Exception as err:
-        logging.error(f"Exception updating inactive chapters: {err}")
-
-    logging.info('BuildStaticWebsiteFiles timer trigger function ran at %s', utc_timestamp)
+    logging.info('BuildStaticWebsiteFiles timer trigger function ran at %s', utc_timestamp)    
 
 class Leader:
     def __init__(self, name, email):
