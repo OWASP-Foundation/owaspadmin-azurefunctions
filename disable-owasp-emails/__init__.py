9e12d62c80fa7a31317d7cae1c003b35e78e6f8d
Harold Blankenship <harold.blankenship@owasp.com>
2022-01-07 17:26:11 -0600

    do not do anything

--- disable-owasp-emails/__init__.py
+++ disable-owasp-emails/__init__.py
@@ -77,86 +77,87 @@ cp = copper.OWASPCopper()
 
 
 def main(mytimer: func.TimerRequest, disableemailverifyqueue: func.Out[func.QueueMessage]) -> None:
-    
-    utc_timestamp = datetime.utcnow().replace(
-        tzinfo=timezone.utc).isoformat()
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+    return   
+    # utc_timestamp = datetime.utcnow().replace(
+    #     tzinfo=timezone.utc).isoformat()
 
-    logging.info('Python timer trigger for disable-owasp-emails function ran at %s', utc_timestamp)
+    # if mytimer.past_due:
+    #     logging.info('The timer is past due!')
 
-    # clean up metadata only
-    setting = os.environ.get('Disable.OWASP.Emails.Cleanup.Stripe.Meta', None)
-    if setting is not None and setting == 'true':
-        cleanup_customer_metadata()
-        return
+    # logging.info('Python timer trigger for disable-owasp-emails function ran at %s', utc_timestamp)
 
-    emails_to_ignore = os.environ.get('Disable.OWASP.Emails.Ignore.Emails', None).replace(' ','').split(',')
+    # # clean up metadata only
+    # setting = os.environ.get('Disable.OWASP.Emails.Cleanup.Stripe.Meta', None)
+    # if setting is not None and setting == 'true':
+    #     cleanup_customer_metadata()
+    #     return
 
-    test_mode = os.environ.get('Disable.OWASP.Emails.Test.Mode', 'true')
-    logging.info(f'Running with test_mode set to {test_mode}')
-    test_users = os.environ.get('Disable.OWASP.Emails.TestUsers', None).replace(' ','').split(',')    
+    # emails_to_ignore = os.environ.get('Disable.OWASP.Emails.Ignore.Emails', None).replace(' ','').split(',')
 
-    try:
-        og = OWASPGoogle()
-        next_page_token = None
-        errors_count = 0
-        msg_list = []
-        while True:
-            google_users = og.GetActiveUsers(next_page_token)
-            next_page_token = google_users.get('nextPageToken')
-            if test_mode == 'true':
-                logging.info('working on page...')
-            for user in google_users['users']:
-                try:
-                    user_email = user['primaryEmail'].lower()
+    # test_mode = os.environ.get('Disable.OWASP.Emails.Test.Mode', 'true')
+    # logging.info(f'Running with test_mode set to {test_mode}')
+    # test_users = os.environ.get('Disable.OWASP.Emails.TestUsers', None).replace(' ','').split(',')    
 
-                    #test logic
-                    if  test_mode == 'true' and user_email not in test_users:
-                        continue
+    # try:
+    #     og = OWASPGoogle()
+    #     next_page_token = None
+    #     errors_count = 0
+    #     msg_list = []
+    #     while True:
+    #         google_users = og.GetActiveUsers(next_page_token)
+    #         next_page_token = google_users.get('nextPageToken')
+    #         if test_mode == 'true':
+    #             logging.info('working on page...')
+    #         for user in google_users['users']:
+    #             try:
+    #                 user_email = user['primaryEmail'].lower()
+
+    #                 #test logic
+    #                 if  test_mode == 'true' and user_email not in test_users:
+    #                     continue
                     
-                    #skip emails that should be ignored
-                    if user_email in emails_to_ignore:
-                        continue
-                    created = datetime.strptime(user['creationTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                    if test_mode == 'false' and ((datetime.today() - created).days < 7):
-                        continue
+    #                 #skip emails that should be ignored
+    #                 if user_email in emails_to_ignore:
+    #                     continue
+    #                 created = datetime.strptime(user['creationTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    #                 if test_mode == 'false' and ((datetime.today() - created).days < 7):
+    #                     continue
     
-                    msg = {
-                        'customer_id': -1,
-                        'email': user_email,
-                        'fullName': user['name']['fullName']
-                    }
+    #                 msg = {
+    #                     'customer_id': -1,
+    #                     'email': user_email,
+    #                     'fullName': user['name']['fullName']
+    #                 }
 
-                    logging.info(f'Adding {user_email} to message list')
-                    msg_list.append(json.dumps(msg))
+    #                 logging.info(f'Adding {user_email} to message list')
+    #                 msg_list.append(json.dumps(msg))
                     
 
-                except Exception as ex:
-                    template = "An exception of type {0} occurred while processing a customer. Arguments:\n{1!r}"
-                    message = template.format(
-                        type(ex).__name__, ex.args)
-                    logging.error(message)
-                    errors_count = errors_count + 1
+    #             except Exception as ex:
+    #                 template = "An exception of type {0} occurred while processing a customer. Arguments:\n{1!r}"
+    #                 message = template.format(
+    #                     type(ex).__name__, ex.args)
+    #                 logging.error(message)
+    #                 errors_count = errors_count + 1
             
-            if not next_page_token:
-                break
+    #         if not next_page_token:
+    #             break
 
-        disableemailverifyqueue.set(msg_list)
+    #     disableemailverifyqueue.set(msg_list)
 
-    except Exception as ex:
-        template = "An exception of type {0} occurred while processing a customer. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        logging.exception(message)
-        raise
+    # except Exception as ex:
+    #     template = "An exception of type {0} occurred while processing a customer. Arguments:\n{1!r}"
+    #     message = template.format(type(ex).__name__, ex.args)
+    #     logging.exception(message)
+    #     raise
 
-def cleanup_customer_metadata():
-    customers = stripe.Customer.list(limit=100)
-    for customer in customers.auto_paging_iter():
-        metadata = customer['metadata']
-        membership_notified = metadata.get('membership_notified', None)
-        if membership_notified is not None:
-            stripe.Customer.modify(customer['id'], metadata={
-                "membership_notified": "", "membership_notified_date": "", "membership_last_notification": ""},)
+# def cleanup_customer_metadata():
+#     customers = stripe.Customer.list(limit=100)
+#     for customer in customers.auto_paging_iter():
+#         metadata = customer['metadata']
+#         membership_notified = metadata.get('membership_notified', None)
+#         if membership_notified is not None:
+#             stripe.Customer.modify(customer['id'], metadata={
+#                "membership_notified": "", "membership_notified_date": "", "membership_last_notification": ""},)
 
