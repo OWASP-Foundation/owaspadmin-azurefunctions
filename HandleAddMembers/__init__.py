02eed8eee9102067ea00f4ca5e844e89f10a9d88
Harold Blankenship <harold.blankenship@owasp.com>
2022-01-10 13:55:40 -0600

    add handle add members

---
+++ HandleAddMembers/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    file = req.params.get('file')
+    if not file:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            file = req_body.get('file')
+
+    if file:
+        return func.HttpResponse(f"Got the file. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
