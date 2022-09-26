37bff6d2422de1865c5c3c962abbf86cfa53403b
hblankenship <harold.blankenship@owasp.com>
2022-09-26 13:20:40 -0500

    update meetup

--- SharedCode/meetup.py
+++ SharedCode/meetup.py
@@ -7,6 +7,10 @@ import logging
 import time
 import random
 from datetime import datetime, timedelta
+import jwt
+from jwt import algorithms
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
 
 class OWASPMeetup:
     meetup_api_url = "https://api.meetup.com"
@@ -22,45 +26,81 @@ class OWASPMeetup:
     def GetHeaders(self):
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.oauth_token}'
+            'Authorization': f'Bearer {self.access_token}'
         }
 
         return headers
 
     def Login(self):
-        login_url  = f"https://secure.meetup.com/oauth2/authorize?scope=basic+event_management+group_content_edit&client_id={os.environ['MU_CONSUMER_KEY']}&redirect_uri={os.environ['MU_REDIRECT_URI']}&response_type=anonymous_code"
-        headers = {
-            'Accept': 'application/json',
-            'X-OAuth-Scopes': 'event_management, basic'
+        now = datetime.utcnow()
+
+        payload_dict = {
+          "sub":os.environ['MU_USER_ID'],
+          "iss":os.environ['MU_CONSUMER_KEY'],
+          "aud":"api.meetup.com",
+          "iat": now,
+          "exp": (now + timedelta(hours=24)).timestamp()
         }
+        jwtheaders = {"kid":os.environ['MU_KEY_ID'],
+                       "alg":"RS256",
+                       "typ":"JWT"}
 
-        res = requests.post(login_url, headers=headers)
-        result = False
-        if '"code":' in res.text:
-            try:
-                json_res = json.loads(res.text)
-                auth_code = json_res['code']
-                login_url  = f"https://secure.meetup.com/oauth2/access?client_id={os.environ['MU_CONSUMER_KEY']}&client_secret={os.environ['MU_SECRET']}&code={auth_code}&redirect_uri={os.environ['MU_REDIRECT_URI']}&grant_type=anonymous_code"
-                res = requests.post(login_url, headers=headers)
-                json_res = json.loads(res.text)
-                self.access_token = json_res['access_token']
-                self.refresh_token = json_res['refresh_token']
+        keystr = os.environ["MU_RSA_KEY"]
+        encoded_key = serialization.load_pem_private_key(keystr.encode(), password=None)#keystr.encode()
+        encoded = jwt.encode(payload=payload_dict, key=encoded_key, algorithm='RS256', headers=jwtheaders)       
+        try:
+            login_url = f"https://secure.meetup.com/oauth2/access"
+            urldata = {
+                 "grant_type" : "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                 "assertion" : encoded
+             }
+            res = requests.post(login_url, data=urldata, headers={'Content-Type': 'application/x-www-form-urlencoded', 'Accept':'application/json'})
+            
+            json_res = json.loads(res.text)
+            self.access_token = json_res['access_token']
+            self.refresh_token = json_res['refresh_token']
 
-                headers = {
-                    'Accept': 'application/json',
-                    'Authorization': f'Bearer {self.access_token}'
-                }
-                login_url = f"https://api.meetup.com/sessions?email={os.environ['MU_USER_NAME']}&password={os.environ['MU_USER_PW']}"
-                res = requests.post(login_url, headers=headers)
-                json_res = json.loads(res.text)
-                self.oauth_token = json_res['oauth_token']
-                self.oauth_token_secret = json_res['oauth_token_secret']
-                result = True
+            result = True
 
-            except:
-                result = False
+        except:
+            result = False
 
-        return result    
+        return result
+
+    # def Login(self):
+    #     login_url  = f"https://secure.meetup.com/oauth2/authorize?scope=basic+event_management+group_content_edit&client_id={os.environ['MU_CONSUMER_KEY']}&redirect_uri={os.environ['MU_REDIRECT_URI']}&response_type=anonymous_code"
+    #     headers = {
+    #         'Accept': 'application/json',
+    #         'X-OAuth-Scopes': 'event_management, basic'
+    #     }
+
+    #     res = requests.post(login_url, headers=headers)
+    #     result = False
+    #     if '"code":' in res.text:
+    #         try:
+    #             json_res = json.loads(res.text)
+    #             auth_code = json_res['code']
+    #             login_url  = f"https://secure.meetup.com/oauth2/access?client_id={os.environ['MU_CONSUMER_KEY']}&client_secret={os.environ['MU_SECRET']}&code={auth_code}&redirect_uri={os.environ['MU_REDIRECT_URI']}&grant_type=anonymous_code"
+    #             res = requests.post(login_url, headers=headers)
+    #             json_res = json.loads(res.text)
+    #             self.access_token = json_res['access_token']
+    #             self.refresh_token = json_res['refresh_token']
+
+    #             headers = {
+    #                 'Accept': 'application/json',
+    #                 'Authorization': f'Bearer {self.access_token}'
+    #             }
+    #             login_url = f"https://api.meetup.com/sessions?email={os.environ['MU_USER_NAME']}&password={os.environ['MU_USER_PW']}"
+    #             res = requests.post(login_url, headers=headers)
+    #             json_res = json.loads(res.text)
+    #             self.oauth_token = json_res['oauth_token']
+    #             self.oauth_token_secret = json_res['oauth_token_secret']
+    #             result = True
+
+    #         except:
+    #             result = False
+
+    #     return result    
 
     def GetGroupEvents(self, groupname, earliest = '', status = ''):
         headers = self.GetHeaders()
