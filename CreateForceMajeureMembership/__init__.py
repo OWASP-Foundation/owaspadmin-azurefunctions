6af78bed16b5511021c6c775d2365553088df87b
hblankenship <harold.blankenship@owasp.com>
2024-03-13 15:48:39 +0000

    uncomment function

--- CreateForceMajeureMembership/__init__.py
+++ CreateForceMajeureMembership/__init__.py
@@ -85,54 +85,54 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     status_code = 405
     result = { "error": "unknown error" }
-    # logging.info("Email: " + email + ", Name: " + name + ", Membership: " + membership_type + ", Country:" + country)
-    # if email and name and membership_type and helperfuncs.is_force_majeure_country(country):
-    #     email = email.lower()
-    #     customers = stripe.Customer.list(email=email)
-    #     s_customer = None
-    #     metadata = None
-    #     end_date = None
-    #     edate = None
-    #     if len(customers) > 0:
-    #         for customer in customers:
-    #             if customer.email == email:
-    #                 s_customer = customer
-    #                 break
-    #     if s_customer:
-    #         metadata = s_customer.get('metadata', None)
-    #     if metadata:
-    #         end_date = metadata.get('membership_end') #only worred about if has an end date (lifetime member is handled in the create_complimentary_member method)
-    #     if end_date:
-    #         try:
-    #             edate = datetime.strptime(end_date, "%m/%d/%Y")
-    #         except:
-    #             edate = datetime.strptime(end_date, "%Y-%m-%d")
+    logging.info("Email: " + email + ", Name: " + name + ", Membership: " + membership_type + ", Country:" + country)
+    if email and name and membership_type and helperfuncs.is_force_majeure_country(country):
+        email = email.lower()
+        customers = stripe.Customer.list(email=email)
+        s_customer = None
+        metadata = None
+        end_date = None
+        edate = None
+        if len(customers) > 0:
+            for customer in customers:
+                if customer.email == email:
+                    s_customer = customer
+                    break
+        if s_customer:
+            metadata = s_customer.get('metadata', None)
+        if metadata:
+            end_date = metadata.get('membership_end') #only worred about if has an end date (lifetime member is handled in the create_complimentary_member method)
+        if end_date:
+            try:
+                edate = datetime.strptime(end_date, "%m/%d/%Y")
+            except:
+                edate = datetime.strptime(end_date, "%Y-%m-%d")
             
 
-    #     if edate is not None and edate > (datetime.today() + timedelta(30)):
-    #         result = {"error": "You already have a membership."}
-    #         status_code = 214
-    #     else:
-    #         # steps to create member
-    #         fname = name[0: name.find(' ')]
-    #         lname = name[name.find(' ') + 1:]
+        if edate is not None and edate > (datetime.today() + timedelta(30)):
+            result = {"error": "You already have a membership."}
+            status_code = 214
+        else:
+            # steps to create member
+            fname = name[0: name.find(' ')]
+            lname = name[name.find(' ') + 1:]
             
-    #         helperfuncs.create_complimentary_member(fname, lname, email, company, country, postal_code, datetime.today().strftime("%Y-%m-%d"), (datetime.today() + timedelta(364)).strftime("%Y-%m-%d"), membership_type, mailing_list, False)
-    #         try:
-    #             cop = OWASPCopper()
-    #             member = cop.FindPersonByEmailObj(email)
-    #             if member:
-    #                 owasp_email = helperfuncs.get_owasp_email(member[0], cop)
-    #                 helperfuncs.unsuspend_google_user(owasp_email)
-    #             else:
-    #                 logging.warn("No member found in copper")        
-    #         except Exception as err:
-    #             logging.error(f'Failed attempting to find and possibly unsuspend Google email: {err}')
+            helperfuncs.create_complimentary_member(fname, lname, email, company, country, postal_code, datetime.today().strftime("%Y-%m-%d"), (datetime.today() + timedelta(364)).strftime("%Y-%m-%d"), membership_type, mailing_list, False)
+            try:
+                cop = OWASPCopper()
+                member = cop.FindPersonByEmailObj(email)
+                if member:
+                    owasp_email = helperfuncs.get_owasp_email(member[0], cop)
+                    helperfuncs.unsuspend_google_user(owasp_email)
+                else:
+                    logging.warn("No member found in copper")        
+            except Exception as err:
+                logging.error(f'Failed attempting to find and possibly unsuspend Google email: {err}')
             
-    #         status_code = 200
-    #         result = { "success": "User created."}  
-    # else:
-    #     result = { "error": f"malformed request : {email}, {name}, {membership_type}" }
+            status_code = 200
+            result = { "success": "User created."}  
+    else:
+        result = { "error": f"malformed request : {email}, {name}, {membership_type}" }
     
     return func.HttpResponse(status_code=status_code, body = json.dumps(result))
         
